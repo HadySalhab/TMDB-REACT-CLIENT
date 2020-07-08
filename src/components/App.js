6d4778e0b2bc6b17e19f94c9b3f4559799f243f8
Hady Salhab <hadysalhab1@gmail.com>
2020-07-08 19:19:35 +1000

    Define Routers

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Create from "./pages/Create";
+import Palette from "./pages/Palette";
+import Movie from "./pages/Movie";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/palette/new" component={Create} />
+				<Route exact path="/palette/:id" component={Palette} />
+				<Route exact path="/movies/:movieName" component={Movie} />
+				<Route component={NotFound} />
+			</Switch>
+		</BrowserRouter>
+	);
 };
 
 export default App;
