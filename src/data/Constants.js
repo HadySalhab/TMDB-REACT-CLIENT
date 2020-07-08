840bcc31b5b1b6bff1c646a67f06a7d4866ab7e9
Hady Salhab <hadysalhab1@gmail.com>
2020-07-08 18:48:31 +1000

    Add Constants.js

---
+++ src/data/Constants.js
@@ -0,0 +1,14 @@
+export const DRAWER_WIDTH = 300;
+export const DRAWER_WIDTH_LARGE = 450;
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
+const IMAGE_FILE_SIZE_W185 = "w185";
+const IMAGE_FILE_SIZE_W250 = "w250_and_h141_face";
+const IMAGE_FILE_SIZE_W1920 = "w1920_and_h800_multi_faces";
+const IMAGE_FILE_SIZE_W600 = "w600_and_h900_bestv2";
+const IMAGE_FILE_SIZE_W276 = "w276_and_h350_face/";
+
+export const IMAGE_W1920 = IMAGE_BASE_URL + IMAGE_FILE_SIZE_W1920;
+export const IMAGE_W600 = IMAGE_BASE_URL + IMAGE_FILE_SIZE_W600;
+export const IMAGE_W185 = IMAGE_BASE_URL + IMAGE_FILE_SIZE_W185;
+export const IMAGE_W250 = IMAGE_BASE_URL + IMAGE_FILE_SIZE_W250;
+export const IMAGE_W276 = IMAGE_BASE_URL + IMAGE_FILE_SIZE_W276;
